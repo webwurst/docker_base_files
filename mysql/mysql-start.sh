4129cbf1ef24db874dbd16161a28da20f2f12c53
Tobias Bradtke <webwurst@gmail.com>
2014-02-26 21:32:55 +0100

    [wip] Some Dockerfiles
    Quite some testing needed.

---
+++ mysql/mysql-start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+chown -R mysql:mysql /var/lib/mysql
+mysql_install_db --user mysql > /dev/null
+
+echo '---- finished: mysql_install_db --user mysql'
+
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-""}
+MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+MYSQL_USER=${MYSQL_USER:-""}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+
+tfile=`mktemp`
+if [[ ! -f "$tfile" ]]; then
+return 1
+fi
+
+cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
+EOF
+
+if [[ $MYSQL_DATABASE != "" ]]; then
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+    if [[ $MYSQL_USER != "" ]]; then
+echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+    fi
+fi
+
+/usr/sbin/mysqld --bootstrap --verbose=0 < $tfile
+rm -f $tfile
+
+echo '---- finished: /usr/sbin/mysqld --bootstrap --verbose=0 < $tfile'
+
+# serf join -replay ${SERF_1_PORT_7946_TCP_ADDR}:${SERF_1_PORT_7946_TCP_PORT}
+exec /usr/sbin/mysqld
